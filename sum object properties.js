33f9542a76398a2ab46f09483db28c964cb92784
iaihabjb <ihab@ihab-baghdadi.com>
2019-09-25 01:53:48 -0400

    sums salaries

---
+++ sum object properties.js
@@ -0,0 +1,12 @@
+// jshint esversion: 6
+// jshint asi: true
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+let sum = 0
+for (const key in salaries) {
+  sum += salaries[key]
+}
+window.alert(sum) // sum is 390
