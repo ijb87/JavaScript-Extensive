f6048b963f8a30c6a09e881c0fb6cb58baed8ad4
iaihabjb <ihab@ihab-baghdadi.com>
2019-09-26 23:00:34 -0400

    isFinite() very useful to check numbers

--- sum numbers from the visitor.js
+++ sum numbers from the visitor.js
@@ -1,6 +1,15 @@
 // jshint esversion: 6
 // jshint asi: true
-const value1 = +window.prompt('Enter a number.')
-const value2 = +window.prompt('Enter another number.')
-const sum = value1 + value2
-window.alert(sum)
+let value = 0
+
+function readNumber () {
+  do {
+    value = window.prompt('Enter a numeric value.')
+  } while (!isFinite(value))
+  if (isFinite(value)) {
+    return value
+  }
+  return null
+}
+
+readNumber()
