98c87b7385293a01c69f6d2436fe2039b1e2552d
iaihabjb <ihab@ihab-baghdadi.com>
2019-09-26 22:40:20 -0400

    unary plus before prompt converts to number

---
+++ sum numbers from the visitor.js
@@ -0,0 +1,6 @@
+// jshint esversion: 6
+// jshint asi: true
+const value1 = +window.prompt('Enter a number.')
+const value2 = +window.prompt('Enter another number.')
+const sum = value1 + value2
+window.alert(sum)
