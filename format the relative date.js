dc668f4bb5092cedbe4ee1ac41cda6a8a96149dc
iaihabjb <ihab@ihab-baghdadi.com>
2019-10-16 16:52:02 -0400

    task in coments

--- format the relative date.js
+++ format the relative date.js
@@ -1,6 +1,13 @@
 // jshint esversion: 6
 // jshint asi: true
 
+// Write a function formatDate(date) that should format date as follows:
+//
+// If since date passed less than 1 second, then "right now".
+// Otherwise, if since date passed less than 1 minute, then "n sec. ago".
+// Otherwise, if less than an hour, then "m min. ago".
+// Otherwise, the full date in the format "DD.MM.YY HH:mm". That is: "day.month.year hours:minutes", all in 2-digit format, e.g. 31.12.16 10:00.
+
 function formatDate (date) {
   const today = new Date()
   const difference = today - date
